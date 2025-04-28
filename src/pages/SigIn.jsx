c4fb58f8a077aeda5e078c324d66158afea52857
Furkan Arslan <furkanarslandev@gmail.com>
2025-04-28 23:35:47 +0300

    performance optimization

--- src/pages/SigIn.jsx
+++ src/pages/SigIn.jsx
@@ -4,13 +4,8 @@ import sign from "../assets/sign4.jpg";
 
 export default function SignIn() {
   return (
-    <div className="relative min-h-screen flex items-center justify-center overflow-hidden">
-      <div
-        style={{
-          backgroundImage: `url(${sign})`,
-        }}
-        className="absolute inset-0 bg-cover bg-center"
-      ></div>
+    <div className="relative min-h-screen flex items-center justify-center overflow-hidden bg-gradient-to-r from-red-600  to-red-900">
+      <div className="absolute inset-0 bg-cover bg-center"></div>
 
       <div className="absolute inset-0 bg-black/40 bg-opacity-60"></div>
 
